86d05256264348502eea13844fe938e6d4394685
João Saraiva <32929768+Saraiv@users.noreply.github.com>
2020-11-30 15:59:26 +0000

    Ja escreve uid na firestore

--- HealthScheduler/public/assets/js/main.js
+++ HealthScheduler/public/assets/js/main.js
@@ -1,3 +1,18 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyDryIOo95hphK_njejU0M5g7iEHT4OY2Ow",
+  authDomain: "healthscheduler-834e9.firebaseapp.com",
+  databaseURL: "https://healthscheduler-834e9.firebaseio.com",
+  projectId: "healthscheduler-834e9",
+  storageBucket: "healthscheduler-834e9.appspot.com",
+  messagingSenderId: "1081449165079",
+  appId: "1:1081449165079:web:ad0897787ad0bd310c0752",
+  measurementId: "G-8WKMXG4700"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 function login(){
 	var email = document.getElementById("email_input");
@@ -6,12 +21,13 @@ function login(){
 	firebase.auth().signInWithEmailAndPassword(email.value, password.value).then((user) => {
 		window.location.assign("mainpage.html");
 
-	}).catch((error) => {
+	})
+	.catch((error) => {
 		var errorCode = error.code;
 		var errorMessage = error.message;
 
 		alert(errorMessage);
-	  });
+	});
 }
 
 function signup() {
@@ -23,17 +39,37 @@ function signup() {
 	const address = document.getElementById("morada_input").value;
 	firebase.auth().createUserWithEmailAndPassword(phoneNumberEmail, passwordRegister)
 	.then((user) => {
-		const userID = "" + user.uid;
-		const response = db.collection('users_medic').doc().set({
-			address,
-			imagePath: "",
-			phoneNumberEmail,
-			userID,
-			username
-		});
+		firebase.auth().signInWithEmailAndPassword(phoneNumberEmail, passwordRegister)
+		.then((user) => {
+			firebase.auth().onAuthStateChanged(function(user) {
+				if (user) {
+				// User is signed in.
+				const uid = "" + user.uid;
+				// ...
+				db.collection('users_medic').doc("" + uid).set({
+					address,
+					imagePath: "",
+					phoneNumberEmail,
+					uid,
+					username
+				});
 
-		loginOnSignup(phoneNumberEmail, passwordRegister);
-	}).catch((error) => {
+				window.location.assign("mainpage.html");
+				} else {
+				// User is signed out.
+				// ...
+				}
+			});
+		})
+		.catch(error => {
+			var errorCode = error.code;
+			var errorMessage = error.message;
+
+			alert(errorMessage);
+		});
+		
+	})
+	.catch((error) => {
 		var errorCode = error.code;
 		var errorMessage = error.message;
 		
@@ -42,33 +78,32 @@ function signup() {
 }
 
 function loginOnSignup(email, password){
-	firebase.auth().signInWithEmailAndPassword(email, password).then((user) => {
-		window.location.assign("mainpage.html");
-
-	}).catch((error) => {
+	firebase.auth().signInWithEmailAndPassword(email, password)
+	.then((user) => {
+		var uid = onLoginAuth();
+		alert(uid + "");
+		return uid;
+	})
+	.catch(error => {
 		var errorCode = error.code;
 		var errorMessage = error.message;
 
 		alert(errorMessage);
-	  });
+	});
 }
 
-function writeUserDataWithCompletion(userId, name, email, imageUrl, address) {
-	firebase.firestore().collection("users").doc(userId + "").set({
-		address: address,
-		imagePath: imageUrl,
-		phoneNumberEmail : email,
-		userID : "",
-		username : name
-	}, function(error) {
-	  if (error) {
-		  alert(error);
-	  } else {
-		// Data saved successfully!
-	  }
-	});
-  }
-
-function fadeIn(){
-	
+function onLoginAuth(){
+	var uid;
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+		  // User is signed in.
+		  alert("" + user.uid);
+		  uid = user.uid;
+		  // ...
+		  return uid + "";
+		} else {
+		  // User is signed out.
+		  // ...
+		}
+	  });
 }
