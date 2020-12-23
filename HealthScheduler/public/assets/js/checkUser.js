c61a922eb7e6ec375ba77dcd526f867432eeb8a2
João Saraiva <32929768+Saraiv@users.noreply.github.com>
2020-12-23 00:33:53 +0000

    Consultas

--- HealthScheduler/public/assets/js/checkUser.js
+++ HealthScheduler/public/assets/js/checkUser.js
@@ -1,13 +1,12 @@
 const db = firebase.firestore();
 
-firebase.auth().onAuthStateChanged(function(user) {
-	if (user != null) {
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user != null) {
         document.getElementsByTagName("BODY")[0].style.display = "contents";
         const uid = "" + user.uid;
-        
-        db.collection('users_medic').doc(uid + "").get().then(function(doc) {
+
+        db.collection('users_medic').doc(uid + "").get().then(function (doc) {
             if (doc.exists) {
-                //console.log("Document data:", doc.data());
                 document.getElementById("img_user").removeAttribute('src');
 
                 var username = document.getElementById("user_name");
@@ -33,42 +32,55 @@ firebase.auth().onAuthStateChanged(function(user) {
             } else {
                 console.log("No such document!");
             }
-        }).catch(function(error) {
+        }).catch(function (error) {
             console.log("Error getting document:", error);
         });
-	} else {
+    } else {
         window.location.replace("login.html");
-	} 
+    }
 });
 
 
-function listConsultas(uid){
-    db.collection("consultas").get().then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            // doc.data() is never undefined for query doc snapshots
-            if(doc.data().medicID == uid){
-                var h1Title = document.createElement("h1");
-                var spanText = document.createElement("span");
+function listConsultas(uid) {
+    db.collection("consultas").get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+            if (doc.data().medicID == uid) {
+                var userID = doc.data().userID;
 
-                h1Title = "" + doc.data().typeOfConsult;
-                spanText = "" + doc.data().notes;
+                var consultaContainer = document.getElementById('allConsultas');
+                var titleOfConsulta = document.createElement('div');
+                titleOfConsulta.className = 'titleOfConsulta';
+                var descriptionOfConsulta = document.createElement('div');
+                descriptionOfConsulta.className = 'descriptionOfConsulta';
 
-                console.log("title: " + h1Title + " text: " + spanText);
-
-                var divTitle = document.getElementById("titleOfConsutla");
-                var divSpan = document.getElementById("descriptionOfConsulta");
-                divTitle.appendChild(
-                    "<h1>" +
-                    h1Title +
-                    "</h1> <br>"
-                );
-
-                divSpan.appendChild(
-                    "<span>" +
-                    spanText +
-                    "</span> <br>"
-                );
+                document.getElementsByClassName('appointementsContainer')[0].appendChild(consultaContainer);
                 
+                
+                consultaContainer.appendChild(titleOfConsulta);
+                consultaContainer.appendChild(descriptionOfConsulta);
+
+
+                var title = document.createElement("h1");
+                var span = document.createElement("span");
+                var hour = document.createElement("p");
+
+                
+
+                var spanText = document.createTextNode(doc.data().notes);
+
+                span.appendChild(spanText);
+
+                var hourText = document.createTextNode(doc.data().hour);
+
+                hour.appendChild(hourText);
+
+                var titleText = document.createTextNode(doc.data().typeOfConsult);
+
+                title.appendChild(titleText);
+
+                titleOfConsulta.append(title);
+                descriptionOfConsulta.append(span);
+                descriptionOfConsulta.append(hour);
             }
         });
     });
