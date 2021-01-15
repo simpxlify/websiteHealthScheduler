38a3dee699a938915b9972ab489753f4fbefba10
João Saraiva <32929768+Saraiv@users.noreply.github.com>
2021-01-15 19:34:53 +0000

    Revert "css do chat minimamente feito"
    This reverts commit 6f54df40403b31c1208f8e639bb596890fad4830.

--- HealthScheduler/public/assets/js/chat.js
+++ HealthScheduler/public/assets/js/chat.js
@@ -66,62 +66,62 @@ db.collection("users").onSnapshot(function (querySnapshot) {
       updateScroll();
       // sendMessage(uid, toId);
       listAllMessages(uid, toId);
-
+      
     });
-
+    
   });
 
-  function updateScroll() {
+  function updateScroll(){
     var element = document.getElementsByClassName("allMessagesList");
     element.scrollTop = element.scrollHeight;
-  }
+}
 
 
   var sendmsg = document.getElementById("sendmsg");
 
+    
 
+    sendmsg.addEventListener("click", function () {
 
-  sendmsg.addEventListener("click", function () {
+      
+      var timeStamp2 = parseInt(Date.now() / 1000);
+      
+      var message = document.getElementById("message").value;
 
+      db.collection('chat_messages').doc(uid).collection(toId).add({
+        "fromId": uid,
+        "message": message,
+        "messageType": text,
+        "toId": toId,
+        "timeStamp": timeStamp2
+      });
+  
+      db.collection('chat_messages').doc(toId).collection(uid).add({
+        "fromId": uid,
+        "message": message,
+        "messageType": text,
+        "toId": toId,
+        "timeStamp": timeStamp2
+      });
 
-    var timeStamp2 = parseInt(Date.now() / 1000);
+      db.collection('latest_messages').doc(uid).collection('latest_message').doc(toId).set({
+        "fromId": uid,
+        "message": message,
+        "messageType": text,
+        "toId": toId,
+        "timeStamp": timeStamp2
+      });
 
-    var message = document.getElementById("message").value;
-
-    db.collection('chat_messages').doc(uid).collection(toId).add({
-      "fromId": uid,
-      "message": message,
-      "messageType": text,
-      "toId": toId,
-      "timeStamp": timeStamp2
+      db.collection('latest_messages').doc(toId).collection('latest_message').doc(uid).set({
+        "fromId": uid,
+        "message": message,
+        "messageType": text,
+        "toId": toId,
+        "timeStamp": timeStamp2
+      });
+  
+        message.value = '';
     });
-
-    db.collection('chat_messages').doc(toId).collection(uid).add({
-      "fromId": uid,
-      "message": message,
-      "messageType": text,
-      "toId": toId,
-      "timeStamp": timeStamp2
-    });
-
-    db.collection('latest_messages').doc(uid).collection('latest_message').doc(toId).set({
-      "fromId": uid,
-      "message": message,
-      "messageType": text,
-      "toId": toId,
-      "timeStamp": timeStamp2
-    });
-
-    db.collection('latest_messages').doc(toId).collection('latest_message').doc(uid).set({
-      "fromId": uid,
-      "message": message,
-      "messageType": text,
-      "toId": toId,
-      "timeStamp": timeStamp2
-    });
-
-    message.value = '';
-  });
 
   userContainer.appendChild(listContainer);
 });
@@ -130,7 +130,7 @@ db.collection("users").onSnapshot(function (querySnapshot) {
 //   var sendmsg = document.getElementById("sendmsg");
 //   // get message
 //   var timeStamp2 = parseInt(Date.now() / 1000);
-
+  
 
 //   // save in database
 //   sendmsg.addEventListener("click", function () {
@@ -159,7 +159,7 @@ db.collection("users").onSnapshot(function (querySnapshot) {
 // listen for incoming messages
 function listAllMessages(uid, toId) {
   var allMessagesContainer = document.getElementById('allMessagesContainer');
-
+  
 
   var listMessages = document.getElementById('allMessagesList');
 
@@ -167,9 +167,7 @@ function listAllMessages(uid, toId) {
 
     listMessages.innerHTML = '';
 
-
     querySnapshot.forEach(function (doc) {
-
       var timeStamp = doc.data().timeStamp;
       var a = new Date(timeStamp * 1000);
       var months = [
@@ -190,9 +188,9 @@ function listAllMessages(uid, toId) {
       var month = months[a.getMonth()];
       var date = a.getDate();
       var hour = a.getHours();
-      hour = (hour < 10) ? '0' + hour : hour;
+      hour = (hour<10) ? '0' + hour : hour;
       var min = a.getMinutes();
-      min = (min < 10) ? '0' + min : min;
+      min = (min<10) ? '0' + min : min;
       var sec = a.getSeconds();
       var formattedTime = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min; //+ ':' + sec;
 
@@ -220,18 +218,6 @@ function listAllMessages(uid, toId) {
       listItem4 = document.createElement('li');
       listItem4.className = 'listItemsMessages';
 
-      divMessageListItem4 = document.createElement('div');
-      divMessageListItem4.className = 'messageWithImage';
-      listItem4.appendChild(divMessageListItem4);
-
-      messageListItem4 = document.createElement('p');
-
-      divMessageListItem4.appendChild(messageListItem4);
-
-      var medicIdForImage = "";
-      medicIdForImage = doc.data().fromId;
-
-      
 
       listItem6 = document.createElement('li');
       listItem6.className = 'listItemsMessages date';
@@ -239,48 +225,8 @@ function listAllMessages(uid, toId) {
       if (doc.data().fromId == uid) {
         listElement.className = 'allMessages myMessage';
         listItem6.className = 'listItemsMessages dateRight';
-        var source = "";
-        var allMessagesImageOfMessage = document.createElement('img');
-
-        db.collection("users_medic")
-        .get()
-        .then(function(querySnapshot) {
-            querySnapshot.forEach(function(doc) {
-              if (medicIdForImage == doc.data().medicID) {
-                console.log("medico");
-                source = doc.data().imagePath;
-                allMessagesImageOfMessage.src = source;
-                console.log(source);
-                listElement.appendChild(allMessagesImageOfMessage);
-              }
-            });
-        })
-        .catch(function(error) {
-            console.log("Error getting documents: ", error);
-        });
       } else {
-        var source = "";
-        var allMessagesImageOfMessage = document.createElement('img');
-        var userIdForImage = doc.data().fromId;
-        console.log(userIdForImage);
-        db.collection("users")
-        .get()
-        .then(function(querySnapshot) {
-            querySnapshot.forEach(function(doc) {
-              if (userIdForImage == doc.data().userID) {
-                console.log("medico");
-                source = doc.data().imagePath;
-                allMessagesImageOfMessage.src = source;
-                console.log(source);
-                listElement.appendChild(allMessagesImageOfMessage);
-              }
-            });
-        })
-        .catch(function(error) {
-            console.log("Error getting documents: ", error);
-        });
-
-        listElement.className = 'allMessages hisMessage';
+        listElement.className = 'allMessages';
       }
 
       // listItem5 = document.createElement('li');
@@ -290,9 +236,7 @@ function listAllMessages(uid, toId) {
       // // Add the item text
       // listItem3.innerHTML = doc.data().fromId;
       // listItem5.innerHTML = doc.data().toId;
-      messageListItem4.innerHTML = doc.data().message;
-
-
+      listItem4.innerHTML = doc.data().message;
       listItem6.innerHTML = formattedTime;
 
 
@@ -301,13 +245,15 @@ function listAllMessages(uid, toId) {
       listElement.appendChild(listItem4);
       // listElement.appendChild(listItem5);
       // listElement.appendChild(listItem3);
-      divMessageListItem4.appendChild(listItem6);
+      listElement.appendChild(listItem6);
 
 
 
     });
     allMessagesContainer.appendChild(listMessages);
+
   });
+
 }
 
 // function deleteMessage(self) {
